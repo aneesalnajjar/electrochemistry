935231b8838391eb18db30150009de32fc8d7faa
Anees Al-Najjar <alnajjaram@ornl.gov>
2023-10-04 09:53:19 -0400

    Update workflow_config.py
    update ACL_WF_client_modules/workflow_config.py

--- ACL_WF_client_modules/workflow_config.py
+++ ACL_WF_client_modules/workflow_config.py
@@ -11,27 +11,27 @@ show_IV_plot=True
 
 ########################################################
 ########################################################
-# server connection ip and port
-ipAddressServer = '10.2.11.161'
-connectionPort='9090'
+# Server connection ip and port
+ipAddressServer = '0.0.0.0'
+connectionPort='0000'
 
 ########################################################
 ########################################################
-# Data trasnfer methods and related paths
+# Data transfer methods and related paths
 profiles_via_data_channel=False # Default is false
 
-# Note: Paths, may be different based on the channel sent through.
+# Note: Paths may be different based on the channel sent through.
 # For example, the testing data paths on the client and server are different if the data_channel is used.
-# The measurements collected on the server/instrument cotroller should point to its local directory. 
-# The client runs on a remote system accesses the measurements across a mounted directory which its path is different. 
+# The measurements collected on the server/instrument controller should point to its local directory. 
+# The client runs on a remote system and accesses the measurements across a mounted directory whose path is different. 
 
 ####################
-# Paths at the cleint side if measurements are sent via the control Channel (using Pyro)
+# Paths at the client side if measurements are sent via the control Channel (using Pyro)
 EoT_Classifier_Path = "./Workflow_dependencies/ml_models_dir"
 training_data_path = "./Workflow_dependencies/training_profiles"
 testing_data_path="./Workflow_dependencies/testing_profiles"
-# Create the directories if they are not exist. 
-# Make sure you place the traing samples in the training_profiles for the first time.
+# Create the directories if they do not exist. 
+# Make sure you place the training samples in the training_profiles for the first time.
 os.makedirs(EoT_Classifier_Path, exist_ok=True) # create EoT_Classifier_Path if it is not exist
 os.makedirs(training_data_path, exist_ok=True) # create training_data_path if it is not exist
 os.makedirs(testing_data_path, exist_ok=True) # create testing_data_path if it is not exist
@@ -40,15 +40,14 @@ os.makedirs(testing_data_path, exist_ok=True) # create testing_data_path if it i
 ######################
 # Paths if measurements are sent via the data Channel over One Drive
 
-# usr='4ua'
-# #usr='3nr'
+# usr='xxx'
 
-# EoT_Classifier_Path = f"C:\\Users\\{usr}\\OneDrive - Oak Ridge National Laboratory\\acl_sp_200\\Workflow_dependencies\\ml_models_dir"
-# training_data_path=f"C:\\Users\\{usr}\\OneDrive - Oak Ridge National Laboratory\\acl_sp_200\\Workflow_dependencies\\training_profiles"
-# testing_data_path=f"C:\\Users\\{usr}\\OneDrive - Oak Ridge National Laboratory\\acl_sp_200\\Workflow_dependencies\\testing_profiles"
+# EoT_Classifier_Path = f"C:\\Users\\{usr}\\OneDrive\\acl_sp_200\\Workflow_dependencies\\ml_models_dir"
+# training_data_path=f"C:\\Users\\{usr}\\OneDrive\\acl_sp_200\\Workflow_dependencies\\training_profiles"
+# testing_data_path=f"C:\\Users\\{usr}\\OneDrive\\acl_sp_200\\Workflow_dependencies\\testing_profiles"
 
-# # Create the directories if they are not exist. 
-# # Make sure you place the traing samples in the training_profiles for the first time.
+# # Create the directories if they do not exist. 
+# # Make sure you place the training samples in the training_profiles for the first time.
 # os.makedirs(EoT_Classifier_Path, exist_ok=True) # create EoT_Classifier_Path if it is not exist
 # os.makedirs(training_data_path, exist_ok=True) # create training_data_path if it is not exist
 # os.makedirs(testing_data_path, exist_ok=True) # create testing_data_path if it is not exist
@@ -56,8 +55,8 @@ os.makedirs(testing_data_path, exist_ok=True) # create testing_data_path if it i
 #####################
 
 # Paths if the measurements are sent via the data Channel using file sharing technique (CIFS)
-# These paths are on the client side which is Linux DGX system 
-# Note this jupyter notebook runs on DGX.
+# These paths are on the client side, which is a Linux DGX system 
+# Note this Jupyter notebook runs on DGX.
 
 # EoT_Classifier_Path = "/mnt/acl_ecosystem/Workflow_dependencies/ml_models_dir"
 # training_data_path="/mnt/acl_ecosystem/Workflow_dependencies/training_profiles"
